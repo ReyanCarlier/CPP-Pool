4e6d3e4cf882382f97888257fd4734e7fde9ccb1
Reyan Carlier <recarlie@student.42.fr>
2022-10-19 17:15:51 +0200

    Deleted useless file.

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -3,14 +3,13 @@
 
 int	main(void)
 {
-	Zombie *horde;
-	int		hordeSize = 5;
+	Zombie			*horde;
+	unsigned int	hordeSize = 5;
 
 	horde = zombieHorde(hordeSize, "MemberOfHorde");
 	for (int i = 0; i < hordeSize; i++)
-	{
 		horde[i].announce();
-	}
+
 	delete[] horde;
 	return (0);
 }
