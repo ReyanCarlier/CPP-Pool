08e0d569a7244363f6227fb6c1fdb45c19fa6ff9
Reyan Carlier <recarlie@student.42.fr>
2022-11-14 09:51:33 +0100

    Deleted useless explicit cast.

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -10,9 +10,11 @@
  */
 int    main(int argc, char **argv)
 {
+	std::string tmp;
+
     for (int i = 1; i < argc; i++)
-        for (size_t j = 0; j < strlen(argv[i]); j++)
-            std::cout << (char)toupper((int)argv[i][j]);
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
     if (argc == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     std::cout << std::endl;
