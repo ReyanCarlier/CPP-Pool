5cf3461aa496d18194748e64eecc4661234426e8
Reyan Carlier <recarlie@student.42.fr>
2022-11-16 17:35:42 +0100

    Improvements.

---
+++ 03/ex00/headers/ClapTrap.hpp
@@ -0,0 +1,45 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# ifndef IOSTREAM_HPP
+#  define IOSTREAM_HPP
+#  include <iostream>
+# endif
+
+# ifndef STRING_HPP
+#  define STRING_HPP
+#  include <string>
+# endif
+
+class ClapTrap {
+	public:
+		ClapTrap(void);
+		ClapTrap(const std::string& name);
+		ClapTrap(const ClapTrap &);
+		~ClapTrap(void);
+		ClapTrap& operator=(ClapTrap const & CT); // Affectation
+
+		void				attack(const std::string& target);
+		void				takeDamage(unsigned int amount);
+		void				beRepaired(unsigned int amount);
+
+		// Getters
+		const std::string	&getName(void);
+		unsigned int		getLP(void);
+		unsigned int		getEP(void);
+		unsigned int		getAD(void);
+
+		// Setters
+		void				setName(const std::string &);
+		void				setLP(unsigned int);
+		void				setEP(unsigned int);
+		void				setAD(unsigned int);
+
+	private:
+		std::string			name;
+		unsigned int		lp;
+		unsigned int		ep;
+		unsigned int		ad;
+};
+
+#endif
