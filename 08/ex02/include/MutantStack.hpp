dd678086d29dc54b6c044802614773b4a669fdf4
Reyan Carlier <recarlie@student.42.fr>
2022-12-01 16:20:59 +0100

    Removed reverse iterators since nothing certified that subjacent container is compatible.

--- 08/ex02/include/MutantStack.hpp
+++ 08/ex02/include/MutantStack.hpp
@@ -17,19 +17,12 @@ class MutantStack : public std::stack<T, Container> {
 		};
 
 		typedef typename std::stack<T, Container>::container_type::iterator iterator;
-		typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
 
 		iterator begin(void) {
 			return this->c.begin();
 		};
 		iterator end(void) {
 			return this->c.end();
-		};
-		reverse_iterator rbegin(void) {
-			return this->c.rbegin();
-		};
-		reverse_iterator rend(void) {
-			return this->c.rend();
 		};
 };
 
--- 08/ex02/src/main.cpp
+++ 08/ex02/src/main.cpp
@@ -1,52 +1,44 @@
 #include "MutantStack.hpp"
 #include <iostream>
 #include <list>
+#include <map>
 
 int main()
 {
 	std::cout << "MutantStack:" << std::endl;
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	MutantStack<std::map<std::string, int> > mstack;
+
+	mstack.push(std::map<std::string, int>());
+	mstack.top()["test"] = 1;
+	mstack.push(std::map<std::string, int>());
+	mstack.top()["test"] = 2;
+	mstack.push(std::map<std::string, int>());
+	mstack.top()["test"] = 3;
+
+	std::cout << "Top: " << mstack.top()["test"] << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
+
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+
+	std::cout << "Top: " << mstack.top()["test"] << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
+
+	mstack.push(std::map<std::string, int>());
+	mstack.top()["test"] = 4;
+
+	std::cout << "Top: " << mstack.top()["test"] << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
+
+	std::cout << "Iterators:" << std::endl;
+	MutantStack<std::map<std::string, int> >::iterator it = mstack.begin();
+	MutantStack<std::map<std::string, int> >::iterator ite = mstack.end();
+
 	++it;
 	--it;
-	while (it != ite)
-	{
-		std::cout << *it << " ";
+	while (it != ite) {
+		std::cout << it->begin()->second << std::endl;
 		++it;
 	}
-	std::cout << std::endl;
-	std::stack<int> s(mstack);
 
-	std::cout << "List:" << std::endl;
-	std::list<int> l;
-	l.push_back(5);
-	l.push_back(17);
-	std::cout << l.back() << std::endl;
-	l.pop_back();
-	std::cout << l.size() << std::endl;
-	l.push_back(3);
-	l.push_back(5);
-	l.push_back(737);
-	l.push_back(0);
-	std::list<int>::iterator it2 = l.begin();
-	std::list<int>::iterator ite2 = l.end();
-	++it2;
-	--it2;
-	while (it2 != ite2)
-	{
-		std::cout << *it2 << " ";
-		++it2;
-	}
-	std::cout << std::endl;
 	return 0;
 }
