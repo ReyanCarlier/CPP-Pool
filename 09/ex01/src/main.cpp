c28e6fe22a30a5a0559b2ecfadde5c56bc8609c6
Reyan Carlier <recarlie@student.42.fr>
2023-03-28 19:37:19 +0200

    Valid version.

--- 09/ex01/src/main.cpp
+++ 09/ex01/src/main.cpp
@@ -3,11 +3,23 @@
 #include <string>
 #include <cstdlib>
 
+/**
+ * @brief Check if the token is an operator
+ * 
+ * @param token 
+ * @return true | false
+ */
 bool isOperator(const std::string &token)
 {
     return token == "+" || token == "-" || token == "*" || token == "/";
 }
 
+/**
+ * @brief Check if the token is a digit
+ * 
+ * @param token 
+ * @return true | false
+ */
 bool isDigit(const std::string &token)
 {
     return token[0] >= '0' && token[0] <= '9';
@@ -20,12 +32,17 @@ int main(int argc, char **argv)
         std::cerr << "Usage: " << argv[0] << " <expression>" << std::endl;
         return 1;
     }
+    if (argv[1][0] == 0)
+    {
+        std::cerr << "Error : Empty expression." << std::endl;
+        return 1;
+    }
     std::string expression = argv[1];
-    std::stack<std::string> stack_operator;
     std::stack<int> stack_operand;
     std::string token;
 
     size_t pos = 0;
+
     for (size_t i = 1; i <= expression.size(); i++)
     {
         token = expression.substr(pos, i - pos);
@@ -37,56 +54,49 @@ int main(int argc, char **argv)
             continue;
         }
         if (isDigit(token))
-        {
             stack_operand.push(std::atoi(token.c_str()));
-        }
         else if (isOperator(token))
         {
-            stack_operator.push(token);
+            char op = token[0];
+
+            if (stack_operand.size() < 2)
+            {
+                std::cerr << "Error : Invalid amount of operands." << std::endl;
+                return 1;
+            }
+            int operand1 = stack_operand.top();
+            stack_operand.pop();
+            int operand2 = stack_operand.top();
+            stack_operand.pop();
+
+            if (op == '+')
+                stack_operand.push(operand2 + operand1);
+            else if (op == '-')
+                stack_operand.push(operand2 - operand1);
+            else if (op == '*')
+                stack_operand.push(operand2 * operand1);
+            else if (op == '/')
+            {
+                if (operand1 == 0) {
+                    std::cerr << "Error : Division by zero." << std::endl;
+                    return 1;
+                }
+                stack_operand.push(operand2 / operand1);
+            }
         }
         else
         {
-            std::cerr << "Invalid token: " << token << std::endl;
+            std::cerr << "Error : Invalid token : " << token << "." << std::endl;
             return 1;
         }
         pos++;
     }
 
-    if (stack_operator.size() != stack_operand.size() - 1)
+    if (stack_operand.size() != 1)
     {
-        std::cerr << "Invalid amount of operators." << std::endl;
+        std::cerr << "Error : Invalid amount of operators." << std::endl;
         return 1;
     }
-    while (!stack_operator.empty())
-    {
-        int operand1 = stack_operand.top();
-        stack_operand.pop();
-        int operand2 = stack_operand.top();
-        stack_operand.pop();
-        std::string op = stack_operator.top();
-        stack_operator.pop();
-        if (op == "+")
-        {
-            stack_operand.push(operand2 + operand1);
-        }
-        else if (op == "-")
-        {
-            stack_operand.push(operand2 - operand1);
-        }
-        else if (op == "*")
-        {
-            stack_operand.push(operand2 * operand1);
-        }
-        else if (op == "/")
-        {
-            if (operand1 == 0)
-            {
-                std::cerr << "Division by zero." << std::endl;
-                return 1;
-            }
-            stack_operand.push(operand2 / operand1);
-        }
-    }
-    std::cout << "Result: " << stack_operand.top() << std::endl;
+    std::cout << stack_operand.top() << std::endl;
     return 0;
 }
